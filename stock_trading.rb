bace02975a45e0a6dbbc5dd63b54546b6529c160
Shail <shailpatel2@gmail.com>
2016-01-04 20:17:49 -0800

    Annotate algo

--- stock_trading.rb
+++ stock_trading.rb
@@ -23,3 +23,8 @@ end
 optimal_sell(first)
 optimal_sell(second)
 optimal_sell(third)
+
+# We greedily walk through the array to track the max profit and lowest price so far. A greedy algrorithm
+# iterates through the problem space taking the optimal solution "so far", until it reaches the end. The
+# greedy approach is only optimal if the problem has "optimal substructure" which means stitching together
+# optimal solutions to subproblems yields an optimal solution.
