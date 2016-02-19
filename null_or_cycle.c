a9a3e97673c71a3baad2d5a6f69f7a3f76765079
Shail <shailpatel2@gmail.com>
2016-02-18 22:24:44 -0800

    Finish null or cycle

--- null_or_cycle.c
+++ null_or_cycle.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodeT {
+    struct nodeT *next;
+    int data;
+} node;
 
 int AcyclicOrCyclic(node *head);
 
-typedef struct nodeT {
-  struct nodeT *next;
-  int data;
-} node;
-
 int AcyclicOrCyclic(node *head) {
-  node *fast = head;
-  node *slow = head;
-  while (1) {
-    if (!fast || !fast->next) {
-      return 0;
-    } else if (fast == slow || fast->next == slow) {
-      return 1;
-    } else {
-      fast = fast->next->next;
-      slow = slow->next;
+    node *fast = head->next;
+    node *slow = head;
+    while (1) {
+        if (!fast || !fast->next) {
+            return 0;
+        } else if (fast == slow || fast->next == slow) {
+            return 1;
+        } else {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
     }
-  }
 }
 
 // Given a linked list that is either NULL-terminated (acyclic) or ends in a cycle (cyclic)
 int main(int argc, char *argv[]) {
-
+    node *head;
+    node *second;
+    node *tail;
+    head = (node *) malloc(sizeof(node));
+    second = (node *) malloc(sizeof(node));
+    tail = (node *) malloc(sizeof(node));
+    head->data = 5;
+    second->data = 6;
+    tail->data = 8;
+    head->next = second;
+    second->next = tail;
+    tail->next = NULL;
+    printf("Value: %d\n", AcyclicOrCyclic(head));
+    tail->next = head;
+    printf("Value: %d\n", AcyclicOrCyclic(head));
 }
