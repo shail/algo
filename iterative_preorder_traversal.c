89c320c998bdb9ded828abcf06021ac8d8e35410
Shail <shailpatel2@gmail.com>
2016-02-20 15:20:05 -0800

    Finished skeleton for an iterative preorder tree traversal

--- iterative_preorder_traversal.c
+++ iterative_preorder_traversal.c
@@ -14,10 +14,30 @@ typedef struct nodeT {
 
 // To do this iteratively, store the right child on an implicit stack and when you run out of children you
 // should pull off the stack
+//
+// Create the stack
+// Push the root node on the stack
+// While stack is not empty
+//  Pop a node
+//  If node is not Null
+//  Print its value
+//  Push the node's right child on the stack
+//  Push the node's left child on the stack
 void PreorderTraversal(node *root) {
+  node *stack;
+  void *data;
+  node *curNode;
 
-}
+  CreateStack(&stack);
+  Push(&stack, root);
 
-int main(int argc, char *argv[]) {
-
+  while (Pop(&stack, &data)) {
+    curNode = (node *) data;
+    if (curNode) {
+      printf("%d\n", curNode->value);
+      Push(&stack, curNode->right);
+      Push(&stack, curNode->left);
+    }
+  }
+  DeleteStack(&stack);
 }
