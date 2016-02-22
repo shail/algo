32ccb0708fe918a92442206962b184fa32b1d8ed
Shail Patel <shailpatel2@gmail.com>
2016-02-22 14:23:13 -0800

    String integer conversion

--- string_integer_conversion.c
+++ string_integer_conversion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // First function should convert an ASCII string to a signed integer, and the second function a signed integer
 // to an ASCII string. A digits numeric value is 'digit' - '0', as it is basically a distance from '0'. The
@@ -8,9 +9,29 @@
 // a new digit. This optimization is called Horner's Rule. If the number is negative you have to set a flag to
 // signify if it is positive or negative and multiply by -1 if it is negative.
 
-int StrToInt(char str[]);
+int StrToInt(char str[]) {
+    int number = 0;
+    int i = 0;
+    int isNeg = 0;
+    if (str[0] == '-') {
+        isNeg = 1;
+        i = 1;
+    }
+    for (; i < strlen(str); i++) {
+        number *= 10;
+        number += (str[i] - '0');
+    }
+
+    if (isNeg) {
+        number *= -1;
+    }
+    return number;
+}
 void IntToStr(int num, char str[]);
 
 int main(int argc, char *argv[]) {
-
+    char str[] = "123\0";
+    char negativeStr[] = "-123\0";
+    printf("Number: %d\n", StrToInt(str));
+    printf("Number: %d\n", StrToInt(negativeStr));
 }
