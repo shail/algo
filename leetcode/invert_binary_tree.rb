e79d573ffb1eed50f3876a9c2d05ceeda60cad04
Shail Patel <shailpatel@yahoo-inc.com>
2016-02-24 15:13:12 -0800

    Invert binary tree

--- leetcode/invert_binary_tree.rb
+++ leetcode/invert_binary_tree.rb
@@ -7,10 +7,27 @@ class TreeNode
 end
 
 def invert_tree(root)
-  temp = root.left
-  root.left = root.right
-  root.right = temp
-  invert_tree(root.right) if !root.right.right.nil?
-  invert_tree(root.left) if !root.left.left.nil?
-  return;
+    if !root.nil?
+        if root.right.nil? && !root.left.nil?
+            root.right = root.left
+            root.left = nil
+            invert_tree(root.right)
+        elsif !root.right.nil? && root.left.nil?
+            root.left = root.right
+            root.right = nil
+            invert_tree(root.left)
+        elsif !root.right.nil? && !root.left.nil?
+            temp = root.left
+            root.left = root.right
+            root.right = temp
+            puts root.right.inspect
+            invert_tree(root.right) if !root.right.right.nil? || !root.right.left.nil?
+            invert_tree(root.left) if !root.left.right.nil? || !root.left.left.nil?
+        else
+            temp = root.left
+            root.left = root.right
+            root.right = temp
+        end
+    end
+    root
 end
