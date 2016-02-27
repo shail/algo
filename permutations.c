83b4a72e218c6dcd880d51b21f1982afe3843acc
Shail <shailpatel2@gmail.com>
2016-02-27 09:52:48 -0800

    Permutations of a string

--- permutations.c
+++ permutations.c
@@ -6,8 +6,49 @@
 // in the input string as a distinct character even if it is repeated. Given the string "aaa", it should print
 // "aaa" six times
 
-int main(int argc, char *argv[]) {
+void DoPermute(char in[], char out[], int used[], int length, int recursLev) {
+  int i;
 
+  if (recursLev == length) {
+    printf("%s\n", out);
+    return;
+  }
+
+  for (i = 0; i < length; i++) {
+    if (used[i]) {
+      continue;
+    }
+
+    out[recursLev] = in[i]; /* put current letter in output */
+    used[i] = 1; /* mark letter as used */
+    DoPermute(in, out, used, length, recursLev + 1);
+    used[i] = 0;
+  }
+}
+
+int Permute(char inString[]) {
+  int length, i, *used;
+  char *out;
+
+  length = strlen(inString);
+  out = (char *) malloc(length + 1);
+  out[length] = '\0';
+  used = (int *) malloc(sizeof(int) * length);
+
+  for (i = 0; i < length; i++) {
+    used[i] = 0;
+  }
+
+  DoPermute(inString, out, used,length, 0);
+
+  free(out);
+  free(used);
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  char *string = "abcd";
+  Permute(string);
 }
 
 // Consider listing all the permutations in alphabetical order. First group of permutations start with "a",
