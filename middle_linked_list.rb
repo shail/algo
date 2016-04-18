eb73b1deb3a7c15d252f955d88f543a3ddd2c675
Shail Patel <shailpatel2@gmail.com>
2016-04-18 09:36:25 -0700

    Find the middle of a linked list with two pointers

---
+++ middle_linked_list.rb
@@ -0,0 +1,37 @@
+class LinkedListNode
+
+    attr_accessor :val, :next
+
+    def initialize(node_value)
+        @val = node_value
+        @next = nil
+    end
+end
+
+def findMiddleNode(pList)
+  slow_pointer = pList
+  fast_pointer = pList
+  while (!fast_pointer.next.nil?) do
+    slow_pointer = slow_pointer.next
+    fast_pointer = fast_pointer.next.next.nil? ? fast_pointer.next : fast_pointer.next.next
+  end
+  slow_pointer.next = nil
+  slow_pointer
+end
+
+head = LinkedListNode.new(1)
+head.next = LinkedListNode.new(2)
+head.next.next = LinkedListNode.new(3)
+
+def create_linked_list(vals)
+  head = LinkedListNode.new(vals.shift)
+  start = head
+  while (!vals.empty?) do
+    start.next = LinkedListNode.new(vals.shift)
+    start = start.next
+  end
+  head
+end
+
+puts findMiddleNode(create_linked_list([1,2])).val.inspect
+puts findMiddleNode(create_linked_list([1,2,3,4,5])).val.inspect
