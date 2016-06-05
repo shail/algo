79dac91f8f3cdcd30fcba83a0b3bc5424de11953
Shail Patel <shailpatel2@gmail.com>
2016-06-05 13:48:14 -0700

    first n natural numbers subset length k recursively

---
+++ n_k_element_combinations.rb
@@ -0,0 +1,29 @@
+def init_combinations(n, k)
+  set = (1..n).to_a
+  recursive_combinations(set, k, 0, [])
+end
+
+def recursive_combinations(set, step_val, array_index, array)
+  if step_val == 0
+    puts array.inspect
+  else
+    (array_index..set.length-1).to_a.each do |i|
+      # Push the current element into the array
+      array.push(set[i])
+      # Recursive call to generate all combinations for the remaining "values" in the combination
+      recursive_combinations(set, step_val - 1, i + 1, array)
+      array.pop()
+    end
+  end
+end
+
+init_combinations(3,2)
+
+# Step_val: how many numbers to print out
+# [0] ==> 0
+# [1], [2], [3] ==> 1
+
+#string parse with + and * alternating
+#random weighted number given hash with key value
+#  - use random number and then go through hash and do sum - random_number and check which is greater than or equal to
+#  - enter ranges in the array and then do a binary search
